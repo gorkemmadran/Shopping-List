e03566a84cbabdaa8ef1787faa4150cfee70be63
Gorkem Madran <gorkemmadran@hotmail.com>
2023-11-23 16:06:52 +0300

    bug fixed

--- shopping-list-start/script.js
+++ shopping-list-start/script.js
@@ -1,35 +1,88 @@
 const form = document.getElementById('item-form')
 const formInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
-form.addEventListener('submit', addItem)
+const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
+
 
 function addItem(e){
-    e.preventDefault();
-    const item = formInput.value
-    if(item === ''){
-        alert('Lütfen bilgileri doldurunuz.')
-        return
-    }
-
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(item))
-
-    const button = createButton('remove-item btn-link text-red')
-    li.appendChild(button)
-    itemList.appendChild(li)
+e.preventDefault()
+let item = formInput.value;
+if(item === ''){
+alert('Listeye birşey eklemek için kutucugu doldurunuz.')
+return
+}
+const li = document.createElement('li');
+li.appendChild(document.createTextNode(item))
+const button = createButton('remove-item btn-link text-red')
+li.appendChild(button)
+itemList.appendChild(li)
+checkUI()
 }
 
 function createButton(clasess){
-const button = document.createElement('button')
-button.className = clasess;
-const icon = createIcon('fa-solid fa-xmark')
-button.appendChild(icon)
-return button;
+    const button = document.createElement('button');
+    button.className = clasess;
+    const icon = createIcon('fa-solid fa-xmark')
+    button.appendChild(icon)
+    return button
 }
-
 
 function createIcon(clasess){
-    const icon = document.createElement('i')
+    const icon = document.createElement('i');
     icon.className = clasess;
-    return icon;
+    return icon
 }
+
+function removeItem(e){
+if(e.target.parentElement.classList.contains('remove-item')){
+    if(confirm('Are you sure? '))
+    e.target.parentElement.parentElement.remove()
+    checkUI()
+
+}
+}
+
+function removeItems(){
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild)
+        checkUI()
+    }
+}
+function filterItems(e){
+ const text = e.target.value.toLowerCase();
+ const items = itemList.querySelectorAll('li')
+ items.forEach(item =>{
+    const itemName = item.firstChild.textContent.toLowerCase();
+    if(itemName.indexOf(text) != -1 ){
+        item.style.display = 'flex'
+    }
+    else{
+        item.style.display = 'none'
+    }
+ })
+
+}
+
+
+ function checkUI(){
+    const items = itemList.querySelectorAll('li')
+    if(items.length === 0){
+        clearBtn.style.display = 'none'
+        itemFilter.style.display = 'none'
+    }else{
+        clearBtn.style.display = 'block'
+        itemFilter.style.display = 'block'
+    }
+ }
+
+
+itemList.addEventListener('click', removeItem)
+form.addEventListener('submit', addItem)
+clearBtn.addEventListener('click', removeItems)
+itemFilter.addEventListener('input', filterItems)
+
+checkUI()
+
+
+
